92cf6e198a27e5d8a1b8b616e44d9797a5a4ca54
Carlos Alberto Martinez Barrera <carlos.martinezb@coppel.com>
2024-07-17 17:46:41 -0700

    Head: Kafka project started Body: 1.-Added main project 2.-Added 2 modules of web services provider/customer Note:need to fix ports on both services

---
+++ src/main/java/com/kafka/SpringForKafka/SpringForKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.SpringForKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForKafkaApplication.class, args);
+	}
+
+}
